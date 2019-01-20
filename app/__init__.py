2be06e1ba2b6d742cd22314614330e4b33c594f8
kburudi <bkevery@gmail.com>
2019-01-20 21:57:38 +0300

    [feature #163222854] update app config

--- app/__init__.py
+++ app/__init__.py
@@ -4,16 +4,24 @@ from flask import Flask, Blueprint, jsonify
 from instance.config import app_config
 from app.api.v2.views.user_views import v2_blue
 from app.api.v2.views.meet_views import v_blue
-from app.api.v2.models.db_connect import db_init
+from app.api.v2.models.db_connect import connect_db, db_init, create_admin
 
 
 def create_app(name_conf):
-    my_app = Flask(__name__)
-    my_app.config['SECRET_KEY'] = os.getenv("SECRET")
+    my_app = Flask(__name__, instance_relative_config=True)
+    my_app.config.from_object(app_config[name_conf])
+    my_app.config.from_pyfile('config.py')
+
+    try:
+        conn = connect_db()
+        db_init(conn)
+        create_admin(conn)
+
+    except Exception as error:
+        print('Error creating tables: {}'.format(str(error)))
+
     my_app.register_blueprint(v2_blue, url_prefix="/api/v2/auth")
     my_app.register_blueprint(v_blue, url_prefix="/api/v2")
-
-    db_init(os.getenv("DB_URL"))
 
     @my_app.errorhandler(404)
     def page_not_found(error):
