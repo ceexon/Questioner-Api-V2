bd8b978b08193b4081a6ec771c31e305866d3062
kburudi <bkevery@gmail.com>
2019-01-22 18:39:21 +0300

    [ft #163222883]update meetups

--- tests/v2/test_meetups.py
+++ tests/v2/test_meetups.py
@@ -160,7 +160,6 @@ class MeetupTest(BaseTest):
         response = self.client.get(
             "api/v2/meetups/1", headers={"x-access-token": admin_token})
         self.assertEqual(response.status_code, 200)
-        print(json.loads(response.data.decode("utf-8")))
 
         """ test rsvp no status """
         response = self.client.post("api/v2/meetups/1/rsvp", headers={
