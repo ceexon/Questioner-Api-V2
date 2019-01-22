f6696307e95a5cc51cf992071a93d87d9e576f0f
kburudi <bkevery@gmail.com>
2019-01-22 18:32:40 +0300

    [ft #163222883]Add basetest

--- tests/v2/test_questions.py
+++ tests/v2/test_questions.py
@@ -3,16 +3,22 @@ import os
 import unittest
 import json
 from tests.v2.test_users import BaseTest, secret
-from tests.v2.test_meetups import MeetupTest
 
 
-class TestQuestions(MeetupTest):
+class TestQuestions(BaseTest):
+    def admin_login(self):
+        response = self.client.post(
+            "api/v2/auth/login", data=json.dumps({"username": "admin", "password": "$$PAss12"}), content_type="application/json")
+        fetch_adm_token = json.loads(response.data.decode("utf-8", secret))
+        admin_token = fetch_adm_token["token"]
+        return admin_token
+
     def test_ask_question_comment_and_vote(self):
         """ test ask fails meetup id not found """
         self.client.post(
             "api/v2/meetups", data=json.dumps(self.meetup_ok), headers={"x-access-token": self.admin_login()}, content_type="application/json")
         response = self.client.post("/api/v2/questions", data=json.dumps(self.question_ask), headers={
-            "x-access-token": self.sign_login_local()}, content_type="application/json")
+            "x-access-token": self.admin_login()}, content_type="application/json")
         result = json.loads(response.data.decode("utf-8"), secret)
         self.assertEqual(response.status_code, 404)
         self.assertEqual(result["error"], "Mettup with id 2 not found")
@@ -20,14 +26,14 @@ class TestQuestions(MeetupTest):
         """ test a successful post """
         self.question_ask["id"] = 3
         response = self.client.post("/api/v2/questions", data=json.dumps(self.question_ask), headers={
-            "x-access-token": self.sign_login_local()}, content_type="application/json")
+            "x-access-token": self.admin_login()}, content_type="application/json")
         result = json.loads(response.data.decode("utf-8"), secret)
         self.assertEqual(response.status_code, 201)
         self.assertTrue(result["data"])
 
         """ test a try to duplicate post """
         response = self.client.post("/api/v2/questions", data=json.dumps(self.question_ask), headers={
-            "x-access-token": self.sign_login_local()}, content_type="application/json")
+            "x-access-token": self.admin_login()}, content_type="application/json")
         result = json.loads(response.data.decode("utf-8"), secret)
         self.assertEqual(response.status_code, 403)
         self.assertEqual(result["error"], "A similar question already exists")
