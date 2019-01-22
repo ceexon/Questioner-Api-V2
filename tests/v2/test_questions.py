96feac7ca89b6fd694f3ac1975587bc0217cfd43
kburudi <bkevery@gmail.com>
2019-01-22 19:01:54 +0300

    [finishes #163222883]update meetup id

--- tests/v2/test_questions.py
+++ tests/v2/test_questions.py
@@ -15,16 +15,17 @@ class TestQuestions(BaseTest):
 
     def test_ask_question_comment_and_vote(self):
         """ test ask fails meetup id not found """
-        self.client.post(
+        meetup = self.client.post(
             "api/v2/meetups", data=json.dumps(self.meetup_ok), headers={"x-access-token": self.admin_login()}, content_type="application/json")
+        get_meetup = json.loads(meetup.data.decode("utf-8"))
+        self.question_ask["id"] = 50
         response = self.client.post("/api/v2/questions", data=json.dumps(self.question_ask), headers={
             "x-access-token": self.admin_login()}, content_type="application/json")
         result = json.loads(response.data.decode("utf-8"), secret)
         self.assertEqual(response.status_code, 404)
-        self.assertEqual(result["error"], "Mettup with id 2 not found")
+        self.assertEqual(result["error"], "Mettup with id 50 not found")
 
         """ test a successful post """
-        self.question_ask["id"] = 3
         response = self.client.post("/api/v2/questions", data=json.dumps(self.question_ask), headers={
             "x-access-token": self.admin_login()}, content_type="application/json")
         result = json.loads(response.data.decode("utf-8"), secret)
