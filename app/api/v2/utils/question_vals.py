39f71dabb41879fd5f315848ab1d103a61086d35
kburudi <bkevery@gmail.com>
2019-01-23 05:20:56 +0300

    [feature #163222887]reduced complexity

--- app/api/v2/utils/question_vals.py
+++ app/api/v2/utils/question_vals.py
@@ -6,7 +6,3 @@ class QuestionValid(BaseValidation):
     """ validate user question input """
 
     question_required = ["meetup", "title", "body"]
-
-    @staticmethod
-    def prevent_duplication(question_data):
-        pass
