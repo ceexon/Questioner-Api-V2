0b76fefd42b708a49ceaeb8b529f710dc4db653a
kburudi <bkevery@gmail.com>
2019-01-23 05:22:08 +0300

    [feature #163222887]pep 8 line standars update

--- tests/v2/test_init.py
+++ tests/v2/test_init.py
@@ -7,17 +7,19 @@ from app.api.v2.models import db_connect
 class TestInitApp(BaseTest):
     def test_wromg_init_methods(self):
         """user signup wrong url """
-        response = self.client.post("api/v2/auth/signup/name",
-                                    data=json.dumps(self.success_signup),
-                                    content_type="application/json")
+        response = self.client.post(
+            "api/v2/auth/signup/name",
+            data=json.dumps(self.success_signup),
+            content_type="application/json")
         result = json.loads(response.data.decode("utf-8"))
         self.assertEqual(result["error"], "Url not found")
         self.assertEqual(response.status_code, 404)
 
         """user signup method not allowed """
-        response = self.client.get("api/v2/auth/signup",
-                                   data=json.dumps(self.success_signup),
-                                   content_type="application/json")
+        response = self.client.get(
+            "api/v2/auth/signup",
+            data=json.dumps(self.success_signup),
+            content_type="application/json")
         result = json.loads(response.data.decode("utf-8"))
         self.assertEqual(result["error"], "Method not allowed")
         self.assertEqual(response.status_code, 405)
