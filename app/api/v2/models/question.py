2afce84c1d64bc5432e13d63cf951e2aa565bb4f
kburudi <bkevery@gmail.com>
2019-01-23 05:23:08 +0300

    [feature #163222887]add model for votes

--- app/api/v2/models/question.py
+++ app/api/v2/models/question.py
@@ -26,3 +26,51 @@ class Question(db_conn):
         WHERE {} = '{}'""".format(search_by, value)
         meetup = db_conn.fetch_single_data_row(db_conn, query)
         return meetup
+
+
+class Voting(db_conn):
+    def __init__(self, user_id, meetup_id, question_id, upvote, downvote, init_votes):
+        self.user = user_id
+        self.meetup = meetup_id
+        self.question = question_id
+        self.upvote = upvote
+        self.downvote = downvote
+        self.votes = init_votes + upvote - downvote
+        self.voted_at = datetime.datetime.utcnow()
+
+    def update_to_votes(self):
+        query = """
+        INSERT INTO votes (user_id, meetup_id, question_id, upvotes, downvotes, votes, voted_at)
+        VALUES('{}', '{}', '{}', '{}', '{}', '{}', '{}')
+        """.format(self.user, self.meetup, self.question, self.upvote, self.downvote, self.votes, self.voted_at)
+        self.save_incoming_data_or_updates(query)
+
+    @staticmethod
+    def get_initial_vote_count(question_id):
+        """ get votes to a given question """
+        query = """
+        SELECT votes FROM votes
+        WHERE question_id = '{}'""".format(question_id)
+        all_voted_user_count = db_conn.fetch_all_tables_rows(db_conn, query)
+        if all_voted_user_count:
+            return all_voted_user_count[-1]
+        else:
+            return 0
+
+    @staticmethod
+    def get_from_questions(que_id):
+        """ get a specific question using its id """
+        query = """
+        SELECT * FROM questions
+        WHERE id = '{}'""".format(que_id)
+        question = db_conn.fetch_single_data_row(db_conn, query)
+        return question
+
+    @staticmethod
+    def get_a_voted_user_(user_id):
+        """ get a specific question using user id voted-user """
+        query = """
+        SELECT question_id FROM votes
+        WHERE user_id = '{}'""".format(user_id)
+        questionId = db_conn.fetch_single_data_row(db_conn, query)
+        return questionId
