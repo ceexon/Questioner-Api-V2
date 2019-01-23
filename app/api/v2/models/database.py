b68a4552bccc802ed1d652e30f569ddde1038011
kburudi <bkevery@gmail.com>
2019-01-23 03:16:01 +0300

    [feature #163222887]fixed all base class issues

--- app/api/v2/models/database.py
+++ app/api/v2/models/database.py
@@ -9,41 +9,41 @@ class DatabaseConnection:
     def __init__(self, db_url):
         """ initialize the class instance to take a database url as a parameter"""
         try:
-            self.conn = psycopg2.connect(db_url)
+            global conn, cur
+            conn = psycopg2.connect(db_url)
+            cur = conn.cursor()
         except Exception as error:
             print('Unable to connect to database:', error)
-        self.cur = self.conn.cursor()
 
     def create_tables_and_admin(self):
         """ creates all tables """
         all_tables_to_create = set_up_tables()
         for query in all_tables_to_create:
-            self.cur.execute(query)
-            self.conn.commit()
+            cur.execute(query)
+            conn.commit()
 
-        create_admin(self.conn)
+        create_admin(conn)
 
     def drop_all_tables(self):
         """ Deletes all tables in the app """
         tables_to_drop = drop_table_if_exists()
         for query in tables_to_drop:
-            self.cur.execute(query)
-            self.conn.commit
-        self.conn.close()
+            cur.execute(query)
+            conn.commit
 
     def fetch_single_data_row(self, query):
         """ retreives a single row of data from a table """
-        self.cur.execute(query)
-        fetchedRow = self.cur.fetchone()
+        cur.execute(query)
+        fetchedRow = cur.fetchone()
         return fetchedRow
 
     def save_incoming_data_or_updates(self, query):
         """ saves data passed as a query to the stated table """
-        self.cur.execute(query)
-        self.conn.commit()
+        cur.execute(query)
+        conn.commit()
 
     def fetch_all_tables_rows(self, query):
         """ fetches all rows of data store """
-        self.cur.execute(query)
-        all_data_rows = self.cur.fetch_all
+        cur.execute(query)
+        all_data_rows = cur.fetchall()
         return all_data_rows
