29a1ffaaf7b683ad4026943ba28ca5a18ecd1211
kburudi <bkevery@gmail.com>
2019-01-24 12:21:17 +0300

    [feature #163218507]validation for dates

--- app/api/v2/utils/meet_validator.py
+++ app/api/v2/utils/meet_validator.py
@@ -1,3 +1,5 @@
+import datetime
+import timestring
 import psycopg2
 from flask import abort, make_response, jsonify
 from app.api.v2.utils.base_vals import BaseValidation
@@ -16,6 +18,26 @@ class MeetValid(BaseValidation):
             pass
         for meet in existing:
             if meet["location"] == meetup["location"] and meet["happen_on"] == happen_on:
-                abort(make_response(jsonify({"status": "409",
-                                             "error": "You may be trying to duplicate a meetup, one with same time and location exists"}), 409))
+                abort(make_response(jsonify({
+                    "status": "409",
+                    "error": "You may be trying to duplicate a meetup, one with same time and location exists"}), 409))
         pass
+
+    @staticmethod
+    def validate_meetup_date_input(date_string):
+        """ Ensure date format is dd/mm/yyyy 1800"""
+        try:
+            input_date = timestring.Date(date_string)
+        except Exception:
+            abort(make_response(jsonify({
+                "error": "invalid date format",
+                "message": "use mm dd yyyy hr:min ",
+                "status": 422}), 422))
+        if input_date < datetime.datetime.now():
+            abort(make_response(jsonify({
+                "status": 422,
+                "error": "Invalid date",
+                "message": "enter a later date"
+            })))
+
+        return date_string
